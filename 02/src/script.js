08c99f2caf5848d514211c7ec0087cd2a2b5f288
viccenzo90454 <viccenzo90454@gmail.com>
2026-05-15 11:09:47 -0300

    Add files via upload

--- 02/src/script.js
+++ 02/src/script.js
@@ -1,10 +1,13 @@
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav')
+    const footer = document.querySelector('footer')
 
     if(window.scrollY > 0){
         nav.classList.add('sc')
+        footer.classList.add('ft')
     }else{
         nav.classList.remove('sc')
+        footer.classList.remove('ft')
     }
 })
 
@@ -14,3 +17,24 @@ function navTo(id){
         block: 'start'
     })
 }
+
+const cards = document.querySelectorAll('#s2, .card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            cards.forEach((card, index) => {
+                setTimeout(() => {
+                    card.classList.add('visible');
+                }, index * 200);
+            });
+        }else{
+            cards.forEach((card) => {
+                card.classList.remove('visible')
+            });
+        }
+    })
+}, {threshold:0.2});
+
+observer.observe(document.querySelector('#s2'))
+
